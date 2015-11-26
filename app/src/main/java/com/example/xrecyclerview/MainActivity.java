a37d37ccd4e675389e4d7f78c9d0ef93018e9da1
hejie <jianghejie888@163.com>
2015-11-26 11:23:21 +0800

    add gif

--- app/src/main/java/com/example/xrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/xrecyclerview/MainActivity.java
@@ -1,16 +1,69 @@
 package com.example.xrecyclerview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private XRecyclerView mRecyclerView;
+    private MyAdapter mAdapter;
+    private ArrayList<String> listData;
+    private int refreshTime = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (XRecyclerView)this.findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setLoadingListener(new XRecyclerView.LoadingListener() {
+            @Override
+            public void onRefresh() {
+                refreshTime ++;
+                new Handler().postDelayed(new Runnable(){
+                    public void run() {
+                        listData.clear();
+                        for(int i = 0; i < 15 ;i++){
+                            listData.add("item" + i + "after " + refreshTime + " times of refresh");
+                        }
+                        mAdapter.notifyDataSetChanged();
+                        mRecyclerView.refreshComplate();
+                    }
+
+                }, 4000);            //refresh data here
+            }
+
+            @Override
+            public void onLoadMore() {
+                new Handler().postDelayed(new Runnable(){
+
+                    public void run() {
+                        for(int i = 0; i < 15 ;i++){
+                            listData.add("item" + (i + listData.size()) );
+                        }
+                        mAdapter.notifyDataSetChanged();
+                        mRecyclerView.loadMoreComplate();
+                    }
+
+                }, 4000);
+
+            }
+        });
+
+        listData = new  ArrayList<String>();
+        mAdapter = new MyAdapter(listData);
+        for(int i = 0; i < 15 ;i++){
+            listData.add("item" + i);
+        }
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -34,4 +87,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/example/xrecyclerview/MyAdapter.java
@@ -0,0 +1,44 @@
+package com.example.xrecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jianghejie on 15/11/26.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    public ArrayList<String> datas = null;
+    public MyAdapter(ArrayList<String> datas) {
+        this.datas = datas;
+    }
+    //创建新View，被LayoutManager所调用
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item,viewGroup,false);
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
+    //将数据与界面进行绑定的操作
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        viewHolder.mTextView.setText(datas.get(position));
+    }
+    //获取数据的数量
+    @Override
+    public int getItemCount() {
+        return datas.size();
+    }
+    //自定义的ViewHolder，持有每个Item的的所有界面元素
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ViewHolder(View view){
+            super(view);
+            mTextView = (TextView) view.findViewById(R.id.text);
+        }
+    }
+}
