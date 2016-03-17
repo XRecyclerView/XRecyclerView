ed1c1d16db896804d2c794e0a84353dc400f52c8
goodboywt <413932965@qq.com>
2016-03-17 10:47:23 +0800

    支持setEmptyView
    支持setEmptyView，类似原来listView的用法，在adapter没有数据的时候，或者没有header的时候，显示emptyView，可以方便的指定一个view，在没有数据时显示。

---
+++ app/src/main/java/com/example/xrecyclerview/EmptyViewActivity.java
@@ -0,0 +1,66 @@
+package com.example.xrecyclerview;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jcodecraeer.xrecyclerview.ProgressStyle;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
+
+import java.util.ArrayList;
+
+public class EmptyViewActivity extends AppCompatActivity {
+    private XRecyclerView mRecyclerView;
+    private MyAdapter mAdapter;
+    private ArrayList<String> listData;
+    private View mEmptyView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview);
+        mRecyclerView = (XRecyclerView)this.findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mEmptyView = findViewById(R.id.text_empty);
+
+        mRecyclerView.setEmptyView(mEmptyView);
+
+        //没有数据，触发emptyView
+        listData = new  ArrayList<String>();
+        mAdapter = new MyAdapter(listData);
+
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
