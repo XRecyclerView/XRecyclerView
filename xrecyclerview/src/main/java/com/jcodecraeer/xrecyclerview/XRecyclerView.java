e2def551b2dc4f88650ff6f4c518ef3751d9764b
koho <ihopeme.hd@gmail.com>
2018-03-01 19:54:19 +0800

    修复事件的不连续导致的自动滚动

--- xrecyclerview/src/main/java/com/jcodecraeer/xrecyclerview/XRecyclerView.java
+++ xrecyclerview/src/main/java/com/jcodecraeer/xrecyclerview/XRecyclerView.java
@@ -402,6 +402,10 @@ public class XRecyclerView extends RecyclerView {
                         break;
                     mRefreshHeader.onMove(deltaY / DRAG_RATE);
                     if (mRefreshHeader.getVisibleHeight() > 0 && mRefreshHeader.getState() < ArrowRefreshHeader.STATE_REFRESHING) {
+                        MotionEvent event = MotionEvent.obtain(ev);
+                        event.setAction(MotionEvent.ACTION_DOWN);
+                        super.onTouchEvent(event);
+                        event.recycle();
                         return false;
                     }
                 }
